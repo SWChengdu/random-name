5503906b50b69e1a38d43f0e10e85b63e78339da
Kiat Lim <kiat@pixelonion.com>
2014-06-12 16:23:46 +0800

    Styled up the random winner page

--- index.php
+++ index.php
@@ -4,17 +4,20 @@
 
 <html>
   <head>
-    <title>DrupalCamp SG 2014</title>
+    <title>DrupalCamp Singapore 2014 Raffle</title>
     <link rel="stylesheet" href="random.css" type="text/css"/>
     <script src="./jquery-1.10.2.min.js"></script>
     <script src="./random.js"></script>
   </head>
 
 <body>
-
-  <h1>DrupalCamp SG 2014 Raffle</h1>
-  <h2>Losers</h2>
-  <ul id="losers">
+  <div class="container">
+    <div class="header">
+      <img src="images/header-banner.jpg">
+    </div>
+  <div class="attendees-wrapper">
+  <h2>Attendees</h2>
+  <ul id="attendees">
     <?php foreach ($names as $name) { ?>
       <?php
         $nameId = preg_replace('/\s+/', '', $name);
@@ -22,11 +25,13 @@
       <li id="<?php echo $nameId; ?>"><?php echo trim($name); ?></li>
     <?php } ?>
   </ul>
-
-  <span id="draw-name">Draw a Name</span>
-  <h2>Winners!</h2>
+</div>
+  <span id="draw-name">Pick a lucky winner</span>
+  <div class="winner-container">
   <ol id="winners">
   </ol>
+</div>
+</div>
 </body>
 
 
--- random.js
+++ random.js
@@ -3,15 +3,16 @@ $(document).ready(function(){
     $("#draw-name").click(function(){
 
         var nameList = [];
-        $("#losers li").each(function() {
+        $("#attendees li").each(function() {
             nameList.push($(this).text())
         });
 
         var winner = nameList[Math.floor(Math.random()*nameList.length)];
         var winnerId = winner.replace(/ /g,'');
         $("#winners").append('<li>' + winner + '</li>');
+        $(".winner-container").show();
 
-        $("#losers li").remove("#" + winnerId);
+        $("#attendees li").remove("#" + winnerId);
 
 
     });
