061577475461228c30647f28367b6a49e29512da
Kiat Lim <kiat@pixelonion.com>
2014-06-12 22:24:19 +0800

    Adjusted markup and added sponsor logo

--- index.php
+++ index.php
@@ -15,26 +15,31 @@
     <div class="header">
       <img src="images/header-banner.jpg">
     </div>
-  <div class="attendees-wrapper">
-  <h2>Attendees</h2>
-  <ul id="attendees">
-    <?php foreach ($names as $name) { ?>
-      <?php
-        $nameId = preg_replace('/\s+/', '', $name);
-      ?>
-      <li id="<?php echo $nameId; ?>"><?php echo trim($name); ?></li>
-    <?php } ?>
-  </ul>
-</div>
-<div id="drumroll-image">
-  <img src="images/drumroll.gif">
-</div>  
-  <span id="draw-name">Pick a lucky winner</span>
-  <div class="winner-container">
-  <ol id="winners" reversed>
-  </ol>
-</div>
-</div>
+    <div class="attendees-wrapper">
+      <h2>Attendees</h2>
+      <ul id="attendees">
+      <?php foreach ($names as $name) { ?>
+        <?php
+          $nameId = preg_replace('/\s+/', '', $name);
+        ?>
+        <li id="<?php echo $nameId; ?>"><?php echo trim($name); ?></li>
+      <?php } ?>
+      </ul>
+    </div>
+    <div id="drumroll-image">
+      <img src="images/drumroll.gif">
+    </div>  
+    <span id="draw-name">Pick a lucky winner</span>
+    <div class="winner-container">
+      <ol id="winners" reversed></ol>
+    </div>
+
+    <div class="sponsors">
+      <ul>
+        <li>Proudly sponsored by <a href="http://www.acquia.com"><img src="images/acquia-logo.png"></a></li>
+      </ul>
+    </div>
+  </div>
 </body>
 
 
