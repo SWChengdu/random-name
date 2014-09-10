052acb0e323360dddc0d7070f57322b4370fce40
Mark Koester <mark@int3c.com>
2014-09-10 17:40:23 +0800

    conversion to use only javascript

--- random.js
+++ random.js
@@ -5,28 +5,18 @@ $(document).ready(function(){
         snd.play();
         $("#drumroll-image").show();
         $("#draw-name").hide();
-
         setTimeout(function (){
             $("#drumroll-image").hide();
             $("#draw-name").show();
-
             var nameList = [];  
             $("#attendees li").each(function() {
-                nameList.push($(this).text())
+                nameList.push($(this).text()+'::'+$(this).attr('id'));
             });
-
             var winner = nameList[Math.floor(Math.random()*nameList.length)];
-            var winnerId = winner.replace(/ /g,'');
-            $("#winners").prepend('<li>' + winner + '</li>');
+            var winner_name = winner.split("::");
+            $("#winners").prepend('<li>' + winner_name[0] + '</li>');
             $(".winner-container").show();
-
-            $("#attendees li").remove("#" + winnerId);
-
+            $("#attendees li").remove("#" + winner_name[1]);
          }, 1800);
-
-
-
-
     });
-
 });
